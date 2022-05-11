c15f1d2eb1f4b3f68330aa1fcc4e6c581a8515ab
Newyear5 <peradonjongjaroonkait@gmail.com>
2022-05-11 22:55:15 +0700

    third

--- base/urls.py
+++ base/urls.py
@@ -11,15 +11,15 @@ urlpatterns = [
     path('room/<str:pk>/',views.room , name = "room"),
     path('room/<str:pk>/liststudent',views.roomliststd , name = "list-student"),
     path('room/<str:pk>/listalert',views.roomlistalertstd , name = "list-alert"),
-    path('student-room/', views.studentRoom, name="student-room"),
-
     path('', views.createRoom, name="create-room"),
     path('update-room/<str:pk>/', views.updateRoom, name="update-room"),
     path('delete-room/<str:pk>/', views.deleteRoom, name="delete-room"),
-
+    
+    path('student-room/', views.studentRoom, name="student-room"),
     path('student-group/', views.studentGroup, name="student-group"),
-    path('student-room/', views.deleteGroup, name="delete-group"),
-    path('student-room-group/<str:pk>/', views.studentRoomGroup, name="student-room-group"),
+    path('delete-group/', views.deleteGroup, name="delete-group"),
+    path('student-in-room/<str:pk>/', views.studentinRoom, name="student-in-room"),
+    path('student-room-record/<str:pk>/', views.studentroomrecord, name="student-room-record"),
     
 
 ]
--- base/views.py
+++ base/views.py
@@ -1,3 +1,4 @@
+from multiprocessing import context
 from django.shortcuts import get_object_or_404, render, redirect
 from django.http import HttpResponse
 from django.contrib import messages
@@ -61,8 +62,12 @@ def home(request):
     if not request.user.is_authenticated:
         return render(request,'base/login_error.html')
 
-    q = request.GET.get('q') if request.GET.get('q') != None else ''
-    rooms = Room.objects.filter(room_name__icontains=q)
+
+    q = request.GET.get('q')
+    if q is not None:
+        rooms = Room.objects.filter(room_name__icontains=q)
+    else:
+        q = ''
     users = CustomUser.objects.filter()
     form = RoomForm(initial={'room_host':request.user})
     room_count = rooms.count()
@@ -98,12 +103,39 @@ def studentRoom(request):
     if not request.user.is_authenticated:
         return render(request,'base/login_error.html')
     
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    rooms = Room.objects.filter(room_name__icontains=q)
     students = Student.objects.get(user=request.user)
     groups = Student_Group.objects.filter(student_id=students)
+
     group_count = groups.count()
 
-    context = {'students':students ,'groups':groups ,'group_count':group_count}
+
+    context = {'students':students ,'groups':groups ,'group_count':group_count ,'rooms':rooms}
     return render(request,'base/Student_room.html',context)
+
+
+def studentinRoom(request,pk):
+    if not request.user.is_authenticated:
+        return render(request,'base/login_error.html')
+    
+
+    students = Student.objects.get(user=request.user)
+    rooms = Room.objects.get(room_id=pk)
+
+    context = {'students':students ,'rooms':rooms}
+    return render(request,'base/student_in_room.html',context)
+
+def studentroomrecord(request,pk):
+    if not request.user.is_authenticated:
+        return render(request,'base/login_error.html')
+
+    students = Student.objects.get(user=request.user)
+    groups = Student_Group.objects.filter(student_id=students)
+    rooms = Room.objects.get(room_id=pk)  
+
+    context = {'students':students,'groups':groups ,'rooms': rooms}
+    return render(request,'base/student_room_record.html',context)
 
 def studentGroup(request):
     student = Student.objects.get(user=request.user)
@@ -122,18 +154,6 @@ def studentGroup(request):
     
     return redirect(request.META.get('HTTP_REFERER'))
 
-def studentRoomGroup(request,pk):
-    if not request.user.is_authenticated:
-        return render(request,'base/login_error.html')
-
-    room = Room.objects.get(room_id=pk)
-    student = Student.objects.get(user=request.user)
-    
-    context = {'room' : room,
-     'student' : student ,
-     }
-    return render(request, 'base/student_room_group.html',context)
-
 def deleteGroup(request):
     if not request.user.is_authenticated:
         return render(request,'base/login_error.html')
@@ -144,8 +164,7 @@ def deleteGroup(request):
         delst_group.delete()
         return redirect('student-room')
         
-    context = {'delst_group': delst_group}
-    return render(request, 'base/Student_room.html', context )
+    return redirect(request.META.get('HTTP_REFERER'))
 
 
 def createRoom(request):
@@ -227,3 +246,6 @@ def roomlistalertstd(request,pk):
 
      }
     return render(request, 'base/room_listalertstudent.html',context)
+
+
+    
